105e95b46365c31ad010f9148a00bcb2d1d4bbdd
Christine Tanabe <tanoobie@icloud.com>
2022-09-01 02:46:11 -0700

    commit tree 67554d9a1986b1388264c5404ccfa70b849b6b5b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+            name: "Sam",
+            streetAddress: "11 Broadway",
+        };
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return{
+        ...obj,
+        [key]:value,
+    };
+}
+
+employee = updateEmployeeWithKeyAndValue();
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+        employee.streetAddress = "12 Broadway";
+
+};
+
+
+
+
+
+ 
